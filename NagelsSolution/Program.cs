3cace26581e01f225b4566bb359dc0391d3f6277
DYLAN ELLIOTT <129302610+Dyl-Elliott@users.noreply.github.com>
2023-04-13 22:04:04 +0100

    Added functionality to retrieve DateTime from within hex string.

--- NagelsSolution/Program.cs
+++ NagelsSolution/Program.cs
@@ -7,12 +7,12 @@ namespace NagelsCodingProblem
     {
         public static void Main(string[] args)
         {
-            string hexString =
-                "A4 E6 16 76 56 C7 32 05 54 B2 04 C7 46 42 E2 E2 E2 E2 E2 E2 E3 9C 15 AF 43 " +
-                "F4 26 17 47 46 C6 52 06 F6 62 04 86 17 37 46 96 E6 77 32 02 00 42 A0 5F 48 " +
-                "04 16 C6 26 57 27 42 04 56 96 E7 37 46 56 96 E2 04 26 F7 26 E2 B6 74 E6 60 " +
-                "C4 16 C6 16 E2 05 47 57 26 96 E6 72 04 56 E6 96 76 D6 12 02 00 8A E5 15 DA " +
-                "94 36 86 57 26 E6 F6 27 96 C2 04 46 97 36 17 37 46 57 22 12 12 3A 35 7F 34";
+            //string hexString =
+            //    "A4 E6 16 76 56 C7 32 05 54 B2 04 C7 46 42 E2 E2 E2 E2 E2 E2 E3 9C 15 AF 43 " +
+            //    "F4 26 17 47 46 C6 52 06 F6 62 04 86 17 37 46 96 E6 77 32 02 00 42 A0 5F 48 " +
+            //    "04 16 C6 26 57 27 42 04 56 96 E7 37 46 56 96 E2 04 26 F7 26 E2 B6 74 E6 60 " +
+            //    "C4 16 C6 16 E2 05 47 57 26 96 E6 72 04 56 E6 96 76 D6 12 02 00 8A E5 15 DA " +
+            //    "94 36 86 57 26 E6 F6 27 96 C2 04 46 97 36 17 37 46 57 22 12 12 3A 35 7F 34";
             //string[] hexValues = hexString.Split(' ');
 
             bool bitA;
@@ -34,6 +34,8 @@ namespace NagelsCodingProblem
             textA = TextValue(hexFormatted1[1]);
 
             shortA = ShortValue(hexFormatted1[2]);
+
+            dateTimeA = DateTimeValue(hexFormatted1[3]);
 
         }
 
@@ -175,6 +177,19 @@ namespace NagelsCodingProblem
             short shortValue = Convert.ToInt16(bits, 2);
 
             return shortValue;
+        }
+
+        public static DateTime DateTimeValue(string hexString)
+        {
+            // set hex as bit array -->
+            var bits = Convert.ToString(Convert.ToInt32(hexString.ToString(), 16), 2).PadLeft(4, '0');
+            // convert bit values to int value -->
+            int intValue = Convert.ToInt32(bits, 2);
+
+            // set dateTime to start from correct date and increment starter value with int value -->
+            DateTime dt = new DateTime(1000, 01, 01).AddDays(intValue);
+
+            return dt;
         }
     }
 }
