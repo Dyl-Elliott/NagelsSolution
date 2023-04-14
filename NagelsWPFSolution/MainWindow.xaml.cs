2efdf8633b4b7c94753d4980a5d698138ea9e33d
DYLAN ELLIOTT <129302610+Dyl-Elliott@users.noreply.github.com>
2023-04-14 18:46:21 +0100

    Created a method which correctly formats variables into their necessary variable types.

--- NagelsWPFSolution/MainWindow.xaml.cs
+++ NagelsWPFSolution/MainWindow.xaml.cs
@@ -26,6 +26,44 @@ namespace NagelsWPFSolution
             hexString.Add(TextATextField.Text);
             hexString.Add(ShortATextField.Text);
             hexString.Add(DateTimeATextField.Text);
+
+            // generate a hexadecimal List<object> with UI data -->
+            List<object> hexValues = HexVariableGenerator(hexString);
+        }
+
+        private static List<object> HexVariableGenerator(List<string> hexString)
+        {
+            List<object> output = new List<object>();
+
+            // bool
+            bool bitA = bool.Parse(hexString[0]);
+            output.Add(bitA);
+
+            // bool
+            bool bitB = bool.Parse(hexString[1]);
+            output.Add(bitB);
+
+            // bool
+            bool bitC = bool.Parse(hexString[2]);
+            output.Add(bitC);
+
+            // bool
+            bool bitD = bool.Parse(hexString[3]);
+            output.Add(bitD);
+
+            // string
+            string textA = hexString[4];
+            output.Add(textA);
+
+            // short
+            short shortA = short.Parse(hexString[5]);
+            output.Add(shortA);
+
+            // DateTime
+            DateTime dateTimeA = DateTime.Parse(hexString[6]);
+            output.Add(dateTimeA);
+
+            return output;
         }
     }
 }
